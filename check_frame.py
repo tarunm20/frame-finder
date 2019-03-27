b289f1fa79eabe918fe778c2c44b05bbac48b210
Tarun Murugan <34909983+tarunm20@users.noreply.github.com>
2019-03-27 10:30:35 +0530

    Add files via upload
    Iterates through a list of images and finds the same image.

---
+++ check_frame.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import os, os.path
+from random import randint
+
+reference = cv2.imread('image.jpg')
+listOfFrames = os.listdir('frames')
+frame_list = []
+
+for frame in listOfFrames:
+    fullPath = os.path.join('/frames',frame)
+    frame_list.append(fullPath)
+
+doppelganger = None
+count = 0
+for i in range(len(frame_list)-1):
+    checking = cv2.imread('frames/frame'+str(i)+'.jpg')
+    if reference.shape == checking.shape:
+        dif = cv2.subtract(reference,checking)
+        b,g,r = cv2.split(dif)
+        count += 1
+        if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
+            doppelganger = cv2.imread('frames/frame'+str(i)+'.jpg')
+            print('frames/frame'+str(i)+'.jpg')
+            break
+
+frame_time = None
+with open('timestamps.csv','r') as f:
+    for line in f:
+        (frame,ms) = line.split(',')
+        if frame == count:
+            frame_time = ms
+            break
+print(frame_time)
+
+cv2.destroyAllWindows()
